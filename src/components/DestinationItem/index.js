52a4b94876e7fbc078071559e920e4e1448f2047
G-kalyankumarreddy <gkalyankuamrreddy1@gmail.com>
2022-06-17 10:16:17 +0000

    destination-search

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {name, imgUrl} = destinationDetails
+  return (
+    <li className="item-container">
+      <img src={imgUrl} alt={name} className="destination-image" />
+      <p className="item-name">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
---
+++ src/components/DestinationSearch/index.js
@@ -0,0 +1,50 @@
+import './index.css'
+
+import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
+
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {destinationsList} = this.props
+    const searchResults = destinationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Destination Search</h1>
+        <div className="search-input-container">
+          <input
+            placeholder="Search"
+            onChange={this.onChangeSearchInput}
+            value={searchInput}
+            className="search-input"
+            type="search"
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+            className="icon-size"
+          />
+        </div>
+        <ul className="destination-list">
+          {searchResults.map(eachDestination => (
+            <DestinationItem
+              destinationDetails={eachDestination}
+              key={eachDestination.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
